ecab1c27a3c882ede0ae0c86ccaff3afcf723560
Yaswanth <yaswanth@MacBook-Air-58.local>
2025-11-24 00:07:51 +0530

    fixed corss

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 app.use(cors({
-  origin: 'https://akshara-backend-p7oln33af.vercel.app',
+  origin: 'https://akshara-academia-ubzk-dmowr7gu4.vercel.app/',
   origin: 'http://localhost:3000',
   credentials: true,
 }));
