90a744d27b73fb2cb70201993e881188db87654e
Yaswanth <yaswanth@MacBook-Air-58.local>
2025-11-24 00:46:42 +0530

    fixed corssssssssa

--- server/server.js
+++ server/server.js
@@ -21,7 +21,10 @@ import { seedFullDatabase } from './utils/seedData.js';
 const app = express();
 dotenv.config();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://aksharaschool.vercel.app", "http://localhost:3000", "http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
 
 // Middleware
